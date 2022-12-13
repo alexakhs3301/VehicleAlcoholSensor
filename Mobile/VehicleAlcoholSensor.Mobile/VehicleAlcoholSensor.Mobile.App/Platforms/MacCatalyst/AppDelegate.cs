62013bb77689108dc2ed79fedb4efb7b50212bd1
Dimitris Vlachopoulos <dvlachopoulos@dvldev.com>
2022-12-13 13:00:04 +0200

    Initial Commit

---
+++ Mobile/VehicleAlcoholSensor.Mobile/VehicleAlcoholSensor.Mobile.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VehicleAlcoholSensor.Mobile.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
